a64f2e485eb5390b6c94dec934c948d7439c6c66
karahbit <gkoubbe@gmail.com>
2015-05-21 02:07:10 -0430

    What I have
    A simple Server-Client (TCP) and a SMTP test

---
+++ George/TCPClient.py
@@ -0,0 +1,11 @@
+from socket import *
+
+serverName = '127.0.0.1'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+sentence = input('Input lowercase sentence:')
+clientSocket.send(bytes(sentence, 'UTF-8'))
+modiefiedSentence = clientSocket.recv(2048).decode('UTF-8')
+print('From Server:', modiefiedSentence)
+clientSocket.close()
